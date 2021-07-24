97c9508fa250e8ae1e7adfbcf1407edd72e3adb4
Nhelv5388 <20c1020010kf@edu.tech.ac.jp>
2021-07-24 21:14:20 +0900

    no message

--- otoge/Assets/Script/Haiti.cs
+++ otoge/Assets/Script/Haiti.cs
@@ -22,7 +22,7 @@ public class Haiti : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log(ScoreEdit.name);
+        
         //マスの指定した分ノーツエディターのシーンを生成する
         if (ScoreEdit == null)
         {
